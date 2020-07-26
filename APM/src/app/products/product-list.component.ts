484d77e37e95e8acb1ca91b874c6be66cead2448
Iván Sainz <ivan.sainz@aheadlabs.com>
2020-07-26 22:28:39 +0200

    Implemented the service-based property bag approach.

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -3,6 +3,7 @@ import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 import {CriteriaComponent} from '../shared/criteria/criteria.component';
+import {ProductParameterService} from './product-parameter.service';
 
 @Component({
     templateUrl: './product-list.component.html',
@@ -10,10 +11,8 @@ import {CriteriaComponent} from '../shared/criteria/criteria.component';
 })
 export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
-    showImage: boolean;
     listFilter: string;
     includeDetail: boolean = true;
-    @ViewChild(CriteriaComponent) filterComponent: CriteriaComponent;
     parentListFilter: string;
 
     imageWidth: number = 50;
@@ -23,19 +22,31 @@ export class ProductListComponent implements OnInit {
     filteredProducts: IProduct[];
     products: IProduct[];
 
-    constructor(private productService: ProductService) { }
+    @ViewChild(CriteriaComponent) filterComponent: CriteriaComponent;
+
+    get showImage(): boolean {
+        return this.productParameterService.showImage;
+    }
+    set showImage(value: boolean) {
+        this.productParameterService.showImage = value;
+    }
+
+    constructor(
+        private productService: ProductService,
+        private productParameterService: ProductParameterService) { }
 
     ngOnInit(): void {
         this.productService.getProducts().subscribe(
             (products: IProduct[]) => {
                 this.products = products;
-                this.performFilter(this.parentListFilter);
+                this.filterComponent.listFilter = this.productParameterService.filterBy;
             },
             (error: any) => this.errorMessage = <any>error
         );
     }
 
     onValueChange(value: string): void {
+        this.productParameterService.filterBy = value;
         this.performFilter(value);
     }
 
